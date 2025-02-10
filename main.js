8086b051ccf9c8d51ce74a7109340809059b6da1
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-10 15:07:01 +0100

    add initial prompt

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("prova log");
+/* 
+#TRACCIA
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+//Ask user for number of km to do and save it in a variable
+const chilometers = Number(prompt("Quanti chilometri vuoi percorrere? Inserisci un numero..."));
+console.log(chilometers);
+
+//Ask user for his age and save it in a variable
+const userAge = Number(prompt("Quanti anni hai? Inserisci un numero..."));
+console.log(userAge);
+
